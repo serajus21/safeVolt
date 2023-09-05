b1525db03f6c99fd3709301918a05029a6180545
Serajus Salehin <serajussalehin21@outlook.com>
2023-09-05 21:02:27 +0600

    filesAdded

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMenu()
+{
+    document.getElementById('show').style.display="none";
+    document.getElementById('hide').style.display="block";
+    document.getElementById('nav').style.right="0%";
+}
+function hideMenu()
+{
+    document.getElementById('hide').style.display="none";
+    document.getElementById('show').style.display="block";
+    document.getElementById('nav').style.right="-100%";
+}
